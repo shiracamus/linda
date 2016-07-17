10af65af550101435b66719f57e5e9ead39e2bf8
Sho Hashimoto <hashimoto@shokai.org>
2016-07-18 02:12:27 +0900

    merge read w/ take

--- src/tuple-space.js
+++ src/tuple-space.js
@@ -15,24 +15,20 @@ export default class TupleSpace {
     this.tuples.push(toTuple(tuple))
   }
 
-  async read (tuple) {
+  async read (tuple, opts = {delete: false}) {
     tuple = toTuple(tuple)
     for (let i = this.tuples.length - 1; i >= 0; i--) {
       let _tuple = this.tuples[i]
       if (tuple.match(_tuple)) {
+        if (opts.delete) {
+          this.tuples = [...this.tuples.slice(0, i), ...this.tuples.slice(i + 1)]
+        }
         return _tuple
       }
     }
   }
 
   async take (tuple) {
-    tuple = toTuple(tuple)
-    for (let i = this.tuples.length - 1; i >= 0; i--) {
-      let _tuple = this.tuples[i]
-      if (tuple.match(_tuple)) {
-        this.tuples = [...this.tuples.slice(0, i), ...this.tuples.slice(i + 1)]
-        return _tuple
-      }
-    }
+    return this.read(tuple, {delete: true})
   }
 }
