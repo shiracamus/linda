74e70c3fb58f7a0320393448670922f857f88ee8
Sho Hashimoto <hashimoto@shokai.org>
2016-07-17 22:12:50 +0900

    Tuple.match

--- src/tuple.js
+++ src/tuple.js
@@ -1,7 +1,17 @@
+import deepEqual from 'deep-equal'
+
 export default class Tuple {
 
   constructor (data) {
     this.data = data
+  }
+
+  match (target) {
+    if (!target.data) return false
+    for (let key of Object.keys(this.data)) {
+      if (!deepEqual(target.data[key], this.data[key])) return false
+    }
+    return true
   }
 
 }
--- test/tuple.js
+++ test/tuple.js
@@ -4,10 +4,24 @@ import Tuple from '../src/tuple'
 import {assert} from 'chai'
 
 describe('Tuple', function () {
+  const name = 'shokai'
+  const web = 'http://shokai.org'
+  const tuple = new Tuple({name, web})
   it('has data', function () {
-    const name = 'shokai'
-    const web = 'http://shokai.org'
-    const tuple = new Tuple({name, web})
     assert.deepEqual(tuple.data, {name, web})
+  })
+
+  describe('match', function () {
+    it('match', function () {
+      assert.ok(tuple.match(new Tuple({name, web})))
+    })
+
+    it('not match', function () {
+      assert.ok(!tuple.match(new Tuple({name})))
+    })
+
+    it('partial match', function () {
+      assert.ok(tuple.match(new Tuple({name, web, age: 100})))
+    })
   })
 })
