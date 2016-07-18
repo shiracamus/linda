9d8402d84da7192f6739a92b65675955d050ddf3
Sho Hashimoto <hashimoto@shokai.org>
2016-07-18 12:48:25 +0900

    async read/take

--- src/tuple-space.js
+++ src/tuple-space.js
@@ -5,6 +5,7 @@ export default class TupleSpace {
   constructor (name) {
     this.name = name
     this.tuples = []
+    this.waiters = []
   }
 
   get size () {
@@ -12,23 +13,35 @@ export default class TupleSpace {
   }
 
   write (tuple) {
-    this.tuples.push(toTuple(tuple))
+    tuple = toTuple(tuple)
+    for (let i = this.waiters.length - 1; i >= 0; i--) {
+      let waiter = this.waiters[i]
+      if (waiter.tuple.match(tuple)) {
+        this.waiters = [...this.waiters.slice(0, i), ...this.waiters.slice(i + 1)]
+        waiter.resolve(tuple)
+        if (waiter.options.delete) return
+      }
+    }
+    this.tuples.push(tuple)
   }
 
-  async read (tuple, opts = {delete: false}) {
+  async read (tuple, options = {delete: false}) {
     tuple = toTuple(tuple)
     for (let i = this.tuples.length - 1; i >= 0; i--) {
       let _tuple = this.tuples[i]
       if (tuple.match(_tuple)) {
-        if (opts.delete) {
+        if (options.delete) {
           this.tuples = [...this.tuples.slice(0, i), ...this.tuples.slice(i + 1)]
         }
         return _tuple
       }
     }
+    return new Promise((resolve) => {
+      this.waiters.unshift({tuple, resolve, options})
+    })
   }
 
-  async take (tuple) {
+  take (tuple) {
     return this.read(tuple, {delete: true})
   }
 }
--- test/tuple-space.js
+++ test/tuple-space.js
@@ -8,29 +8,63 @@ describe('TupleSpace', function () {
   const name = 'shokai'
   const web = 'http://shokai.org'
 
-  let ts = new TupleSpace()
-  ts.write({name, web})
-  describe('write', function () {
-    it('write', function () {
-      assert.equal(ts.size, 1)
+  describe('synchronous case', function () {
+    let ts = new TupleSpace()
+    ts.write({name, web})
+    describe('write', function () {
+      it('write', function () {
+        assert.equal(ts.size, 1)
+      })
+    })
+
+    describe('read', function () {
+      it('read', async function () {
+        let tuple = await ts.read({name})
+        assert.deepEqual(tuple.data, {name, web})
+        assert.equal(ts.size, 1)
+      })
+    })
+
+    describe('take', function () {
+      it('take', async function () {
+        ts.write({name: 'kazusuke'})
+        assert.equal(ts.size, 2)
+        let tuple = await ts.take({name})
+        assert.deepEqual(tuple.data, {name, web})
+        assert.equal(ts.size, 1)
+        tuple = await ts.take({name: 'kazusuke'})
+        assert.deepEqual(tuple.data, {name: 'kazusuke'})
+        assert.equal(ts.size, 0)
+      })
     })
   })
 
-  describe('read', function () {
+  describe('asynchronous case', function () {
     it('read', async function () {
+      let ts = new TupleSpace()
+      setTimeout(() => ts.write({name, web}), 1000)
       let tuple = await ts.read({name})
       assert.deepEqual(tuple.data, {name, web})
       assert.equal(ts.size, 1)
     })
-  })
 
-  describe('take', function () {
     it('take', async function () {
-      ts.write({name: 'kazusuke'})
-      assert.equal(ts.size, 2)
+      let ts = new TupleSpace()
+      setTimeout(() => ts.write({name, web}, 1000))
       let tuple = await ts.take({name})
       assert.deepEqual(tuple.data, {name, web})
-      assert.equal(ts.size, 1)
+      assert.equal(ts.size, 0)
+    })
+
+    it('read then take', async function () {
+      let ts = new TupleSpace()
+      setTimeout(() => ts.write({name, web}, 1000))
+      let [readTuple, takeTuple] = await Promise.all([
+        ts.read({name}), ts.take({name})
+      ])
+      assert.deepEqual(readTuple.data, {name, web})
+      assert.deepEqual(takeTuple.data, {name, web})
+      assert.equal(ts.size, 0)
     })
   })
 })
